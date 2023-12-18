0464e183bbeaa8fd45ddbe8b08b0dccbd59a8ae6
piyushkumar079 <143178077+piyushkumar079@users.noreply.github.com>
2023-12-18 22:53:21 +0530

    hh

---
+++ assignments-master/assignments-master/01-js/easy/expenditure-analysis.js
@@ -0,0 +1,32 @@
+/*
+  Implement a function `calculateTotalSpentByCategory` which takes a list of transactions as parameter
+  and return a list of objects where each object is unique category-wise and has total price spent as its value.
+  Transaction - an object like { itemName, category, price, timestamp }.
+  Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
+*/
+
+function calculateTotalSpentByCategory(transactions) {
+
+  const resultant = {};
+
+  for (let i = 0; i < transactions.length; i++) {
+    const { category, price } = transactions[i];
+
+    if (resultant["category"]!=category)
+     { resultant["category"] = category;
+      resultant["totalSpent"] = 0;}
+    else {
+      resultant["totalSpent"] += price;
+    }
+  }
+  const result=[];
+  
+  for (category in resultant)
+  {
+    result.push({category:resultant[category]});
+  }
+  console.log(result)
+  return result;
+}
+
+module.exports = calculateTotalSpentByCategory;
